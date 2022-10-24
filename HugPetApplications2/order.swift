b838aa0dd114ec41e54b697495b8c4cc19e6e420
abrar <llabrrar@gmail.com>
2022-10-24 18:12:32 +0300

    Update And Add File

--- HugPetApplications2/order.swift
+++ HugPetApplications2/order.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct order: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+        Text("Hello, World!")
+
     }
 }
 
