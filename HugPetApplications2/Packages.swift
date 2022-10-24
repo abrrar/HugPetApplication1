4de43fa4cc239335774459206624c39189a31dc1
Ahad Albaqami <a.albaqami@lean.sa>
2022-10-24 18:29:16 +0300

    Hello its Working

---
+++ HugPetApplications2/Packages.swift
@@ -0,0 +1,21 @@
+//
+//  Packages.swift
+//  HugPetApplications2
+//
+//  Created by Ahad Obaid Albaqami on 28/03/1444 AH.
+//
+
+import SwiftUI
+
+struct Packages: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.yellow)
+    }
+}
+
+struct Packages_Previews: PreviewProvider {
+    static var previews: some View {
+        Packages()
+    }
+}
